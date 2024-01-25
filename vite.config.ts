1a47b4e048a941fe330b15dfac920972fda7aa38
Ethan <ethangely@free.fr>
2024-01-25 20:31:25 +0100

    should work

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,40 @@ import {VitePWA} from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
       react(),
-      VitePWA({registerType: 'autoUpdate'})
+      VitePWA({
+          registerType: 'autoUpdate',
+          manifest: {
+              "theme_color": "#f69435",
+              "background_color": "#f69435",
+              "display": "browser",
+              "scope": "/",
+              "start_url": "/",
+              "name": "firstpwa",
+              "short_name": "firstpwa",
+              "icons": [
+                  {
+                      "src": "/icon-192x192.png",
+                      "sizes": "192x192",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/icon-256x256.png",
+                      "sizes": "256x256",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/icon-384x384.png",
+                      "sizes": "384x384",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "/icon-512x512.png",
+                      "sizes": "512x512",
+                      "type": "image/png"
+                  }
+              ]
+          }
+      })
   ],
 })
 
